96acd68c8340569a44ac7c873b9caef4c9d05634
helliany <ol.yurmanova@gmail.com>
2022-06-08 23:49:05 +0300

    Continue adding tests

--- src/redux/tests/breedsSlice.test.ts
+++ src/redux/tests/breedsSlice.test.ts
@@ -1,7 +1,10 @@
 import breedsReducer, {
+  getBreedsAsync,
   IBreeds,
   setSelected,
 } from '../breedsSlice';
+import axios from 'axios';
+import { configureStore } from '@reduxjs/toolkit';
 
 describe('breeds reducer', () => {
   const initialState: IBreeds = {
@@ -16,5 +19,23 @@ describe('breeds reducer', () => {
   it('should handle selectedBreed', () => {
     const actual = breedsReducer(initialState, setSelected('beng'));
     expect(actual.selectedBreed).toEqual('beng');
+  });
+
+  it('should pass', async () => {
+    const postSpy = jest.spyOn(axios, 'get').mockResolvedValueOnce('first call');
+    const store = configureStore({
+      reducer: function (state = '', action) {
+        switch (action.type) {
+          case 'breeds/fetchData/fulfilled':
+            return action.payload;
+          default:
+            return state;
+        }
+      },
+    });
+    await store.dispatch(getBreedsAsync());
+    expect(postSpy).toHaveBeenCalled();
+    const state = store.getState();
+    expect(state).toEqual([]);
   });
 });
